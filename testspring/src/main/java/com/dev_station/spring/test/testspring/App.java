eb34d6ef31ffb6ba34ed8c90f3b2a0c03baeff98
Shekhar Ullah <ullah.shekhar@gmail.com>
2017-05-31 12:13:23 +0600

    first commit

---
+++ testspring/src/main/java/com/dev_station/spring/test/testspring/App.java
@@ -0,0 +1,13 @@
+package com.dev_station.spring.test.testspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
